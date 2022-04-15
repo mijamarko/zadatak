9b3fd035e421129072ff64a9f863faf82d497079
Marko Mijajlović <marko.b.mijajlovic@gmail.com>
2022-04-15 11:41:08 +0200

    added main context listener

---
+++ src/main/java/it/engineering/web/zadatak/listeners/MainContextListener.java
@@ -0,0 +1,36 @@
+package it.engineering.web.zadatak.listeners;
+
+import java.util.ArrayList;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import it.engineering.web.zadatak.entities.User;
+
+/**
+ * Application Lifecycle Listener implementation class MainContextListener
+ *
+ */
+public class MainContextListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public MainContextListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         sce.getServletContext().setAttribute("loggedInUsers", new ArrayList<User>());
+    }
+	
+}
